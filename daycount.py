b8bb39a04d6a19278fd50177e58bcd9a86e9cc09
Maulik Desai <maulik.desai.quantfin@gmail.com>
2024-01-05 22:24:51 -0500

    added three more day count conventions

--- daycount.py
+++ daycount.py
@@ -1,4 +1,32 @@
 import datetime
+import calendar
+
+
+def _next_leap_day(dt: datetime):
+    if dt.month == 2 and dt.day == 29:
+        return _ensure_leap_day(dt.year + 4)
+
+    if calendar.isleap(dt.year) and dt.month <= 2:
+        return datetime.datetime(dt.year, 2, 29)
+
+    return _ensure_leap_day(((dt.year // 4) * 4) + 4)
+
+
+def _ensure_leap_day(year: int):
+    return datetime.datetime(year, 2, 29) if year.isleap(year) else datetime.datetime(year + 4, 2, 29)
+
+
+def _number_of_leap_days(d1: datetime, d2: datetime):
+    res = 0
+    cur = _next_leap_day(d1)
+    while cur <= d2:
+        cur = _next_leap_day(cur)
+        res += 1
+    return res
+
+
+def _ordinal_diff(d1: datetime, d2: datetime):
+    return d2.toordinal() - d1.toordinal()
 
 
 class DayCount(object):
@@ -13,27 +41,41 @@ class DayCount(object):
     def days(self, d1: datetime, d2: datetime):
         return self._days(d1, d2)
 
-    @staticmethod
-    def _day_count(d1: datetime, d2: datetime):
-        return d2.toordinal() - d1.toordinal()
-
     def _actual_365_fixed_days(self, d1: datetime, d2: datetime):
-        return self._day_count(d1, d2)
+        return _ordinal_diff(d1, d2)
 
     def _actual_365_fixed_year_fraction(self, d1: datetime, d2: datetime):
         return self._actual_365_fixed_days(d1, d2) / 365.0
 
     def _actual_364_days(self, d1: datetime, d2: datetime):
-        return self._day_count(d1, d2)
+        return _ordinal_diff(d1, d2)
 
     def _actual_364_year_fraction(self, d1: datetime, d2: datetime):
         return self._actual_364_days(d1, d2) / 364.0
 
     def _actual_365_25_days(self, d1: datetime, d2: datetime):
-        return self._day_count(d1, d2)
+        return _ordinal_diff(d1, d2)
 
     def _actual_365_25_year_fraction(self, d1: datetime, d2: datetime):
         self._actual_365_25_days(d1, d2) / 365.25
+
+    def _actual_360_days(self, d1: datetime, d2: datetime):
+        return _ordinal_diff(d1, d2)
+
+    def _actual_360_year_fraction(self, d1: datetime, d2: datetime):
+        return self._actual_360_days(d1, d2) / 360.0
+
+    def _no_leap_365_days(self, d1: datetime, d2: datetime):
+        return _ordinal_diff(d1, d2) - _number_of_leap_days(d1, d2)
+
+    def _no_leap_365_year_fraction(self, d1: datetime, d2: datetime):
+        return self._no_leap_365_days(d1, d2) / 365.0
+
+    def _no_leap_360_days(self, d1: datetime, d2: datetime):
+        return _ordinal_diff(d1, d2) - _number_of_leap_days(d1, d2)
+
+    def _no_leap_360_year_fraction(self, d1: datetime, d2: datetime):
+        return self._no_leap_360_days(d1, d2) / 360.0
 
 
 if __name__ == '__main__':
