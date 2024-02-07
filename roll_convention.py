728ac2acc464f18d78db34e59e3d233c37c93aaa
Maulik Desai <maulik.desai.quantfin@gmail.com>
2024-02-06 21:43:58 -0500

    finished implementing standard roll conventions

--- roll_convention.py
+++ roll_convention.py
@@ -17,17 +17,82 @@ class RollConventionType(enum.Enum):
     SFE = enum.auto()
     TBILL = enum.auto()
 
+    DAY_1 = enum.auto()
+    DAY_2 = enum.auto()
+    DAY_3 = enum.auto()
+    DAY_4 = enum.auto()
+    DAY_5 = enum.auto()
+    DAY_6 = enum.auto()
+    DAY_7 = enum.auto()
+    DAY_8 = enum.auto()
+    DAY_9 = enum.auto()
+    DAY_10 = enum.auto()
+    DAY_11 = enum.auto()
+    DAY_12 = enum.auto()
+    DAY_13 = enum.auto()
+    DAY_14 = enum.auto()
+    DAY_15 = enum.auto()
+    DAY_16 = enum.auto()
+    DAY_17 = enum.auto()
+    DAY_18 = enum.auto()
+    DAY_19 = enum.auto()
+    DAY_20 = enum.auto()
+    DAY_21 = enum.auto()
+    DAY_22 = enum.auto()
+    DAY_23 = enum.auto()
+    DAY_24 = enum.auto()
+    DAY_25 = enum.auto()
+    DAY_26 = enum.auto()
+    DAY_27 = enum.auto()
+    DAY_28 = enum.auto()
+    DAY_29 = enum.auto()
+    DAY_30 = enum.auto()
+    DAY_31 = enum.auto()
+
+    WEEKDAY_MON = enum.auto()
+    WEEKDAY_TUE = enum.auto()
+    WEEKDAY_WED = enum.auto()
+    WEEKDAY_THU = enum.auto()
+    WEEKDAY_FRI = enum.auto()
+    WEEKDAY_SAT = enum.auto()
+    WEEKDAY_SUN = enum.auto()
+
 
 class RollConvention(object):
     def __init__(self, name: RollConventionType):
-        self.name = name.name
-        getattr(self, f'setup_{self.name.lower()}')()
+        self.name = name
+
+        if self.name in (RollConventionType.IMMCAD, RollConventionType.IMMAUD, RollConventionType.TBILL):
+            getattr(self, f'setup_{self.name.name.lower()}')()
 
     def matches(self, dt: datetime) -> bool:
+        if self.name.name.startswith('DAY_'):
+            day = int(self.name.name.split('_')[-1])
+            return dt.day == day or \
+                (dt.month == 2 and day >= calendar.monthrange(dt.year, dt.month)[1] and
+                 dt.day == calendar.monthrange(dt.year, dt.month)[1])
+        elif self.name.name.startswith('WEEKDAY_'):
+            day = self.name.name.split('_')[-1]
+            return dt.weekday() == {'MON': 0, 'TUE': 1, 'WED': 2, 'THU': 3, 'FRI': 4, 'SAT': 5, 'SUN': 6}[day]
         return self.adjust(dt) == dt
 
     def adjust(self, dt: datetime) -> datetime:
-        return getattr(self, f'adjust_{self.name.lower()}')(dt)
+        if self.name == RollConventionType.DAY_31:
+            return self.adjust_eom(dt)
+        elif self.name.name.startswith('DAY_'):
+            day = int(self.name.name.split('_')[-1])
+            if day >= 29 and dt.month == 2:
+                return datetime.datetime(dt.year, dt.month, calendar.monthrange(dt.year, dt.month)[1])
+            return datetime.datetime(dt.year, dt.month, day)
+        elif self.name.name.startswith('WEEKDAY_'):
+            day = int(self.name.name.split('_')[-1])
+            day = {'MON': 0, 'TUE': 1, 'WED': 2, 'THU': 3, 'FRI': 4, 'SAT': 5, 'SUN': 6}[day]
+            cur = dt
+            while cur.weekday() != day:
+                cur += relativedelta(days=1)
+            return cur
+        else:
+            return getattr(self, f'adjust_{self.name.name.lower()}')(dt)
 
     def next(self, dt: datetime, delta: relativedelta) -> datetime:
         calculated = self.adjust(dt + delta)
@@ -41,20 +106,12 @@ class RollConvention(object):
             calculated = self.adjust(dt - relativedelta(months=1))
         return calculated
 
-    def setup_none(self):
-        pass
 
     def adjust_none(self, dt: datetime) -> datetime:
         return dt
 
-    def setup_eom(self):
-        pass
-
     def adjust_eom(self, dt: datetime) -> datetime:
         return datetime.datetime(dt.year, dt.month, calendar.monthrange(dt.year, dt.month)[1])
-
-    def setup_imm(self):
-        pass
 
     def adjust_imm(self, dt: datetime) -> datetime:
         return day_of_week_in_month(dt.year, dt.month, 2, 3)
@@ -73,17 +130,11 @@ class RollConvention(object):
     def adjust_immaud(self, dt: datetime) -> datetime:
         return self.ausy.previous(day_of_week_in_month(dt.year, dt.month, 4, 2))
 
-    def setup_immnzd(self):
-        pass
-
     def adjust_immnzd(self, dt: datetime) -> datetime:
         cur = datetime.datetime(dt.year, dt.month, 9)
         while cur.weekday() != 2:
             cur += relativedelta(days=1)
         return cur
-
-    def setup_sfe(self):
-        pass
 
     def adjust_sfe(self, dt: datetime) -> datetime:
         return day_of_week_in_month(dt.year, dt.month, 4, 2)
