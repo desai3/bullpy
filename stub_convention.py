1eb1db26ce9936b8342aca4dd8443fc7f926d982
Maulik Desai <maulik.desai.quantfin@gmail.com>
2024-05-01 14:58:21 -0400

    stub convention implemented, start working on the test

--- stub_convention.py
+++ stub_convention.py
@@ -1,7 +1,10 @@
+import calendar
 import enum
 import datetime
 
-from frequency import plus_days
+from frequency import plus_days, Frequency
+from roll_convention import RollConvention, RollConventionType
+
 
 class StubConventionType(enum.Enum):
     NONE = enum.auto()
@@ -31,7 +34,7 @@ class StubConvention(object):
     def _none_initial_is_stub_long(self, dt1: datetime, dt2: datetime) -> bool:
         return False
 
-    def _short_initial_to_implicit(self, explicit_initial_stub: bool , explicit_final_stub: bool):
+    def _short_initial_to_implicit(self, explicit_initial_stub: bool, explicit_final_stub: bool):
         if explicit_final_stub:
             raise ValueError('Dates specifiy an explicit final stub, but stub convention is SHORT_INITIAL')
 
@@ -43,7 +46,7 @@ class StubConvention(object):
     def _short_initial_is_stub_long(self, dt1: datetime, dt2: datetime) -> bool:
         return False
 
-    def _long_initial_to_implicit(self, explicit_initial_stub: bool , explicit_final_stub: bool):
+    def _long_initial_to_implicit(self, explicit_initial_stub: bool, explicit_final_stub: bool):
         if explicit_final_stub:
             raise ValueError('Dates specifiy an explicit final stub, but stub convention is LONG_INITIAL')
 
@@ -55,7 +58,7 @@ class StubConvention(object):
     def _long_initial_is_stub_long(self, dt1: datetime, dt2: datetime) -> bool:
         return True
 
-    def _smart_initial_to_implicit(self, explicit_initial_stub: bool , explicit_final_stub: bool):
+    def _smart_initial_to_implicit(self, explicit_initial_stub: bool, explicit_final_stub: bool):
         if explicit_final_stub:
             if explicit_initial_stub:
                 return StubConvention(StubConventionType.BOTH)
@@ -66,6 +69,7 @@ class StubConvention(object):
                 return StubConvention(StubConventionType.NONE)
             else:
                 return StubConvention(StubConventionType.SMART_INITIAL)
+
     def _smart_initial_is_stub_long(self, dt1: datetime, dt2: datetime) -> bool:
         return False
 
@@ -113,3 +117,50 @@ class StubConvention(object):
 
     def _both_final_is_stub_long(self, dt1: datetime, dt2: datetime) -> bool:
         return False
+
+    def is_smart(self):
+        return self._type == StubConventionType.SMART_INITIAL.name or self._type == StubConventionType.SMART_FINAL.name
+
+    def is_short(self):
+        return self._type == StubConventionType.SHORT_INITIAL.name or self._type == StubConventionType.SHORT_FINAL.name
+
+    def is_long(self):
+        return self._type == StubConventionType.LONG_INITIAL.name or self._type == StubConventionType.LONG_FINAL.name
+
+    def is_final(self):
+        return self._type == StubConventionType.SHORT_FINAL.name or \
+            self._type == StubConventionType.LONG_FINAL.name or \
+            self._type == StubConventionType.SHORT_FINAL.name
+
+    def is_calculate_forwards(self):
+        return self._type == StubConventionType.SHORT_FINAL.name or \
+            self._type == StubConventionType.LONG_FINAL.name or \
+            self._type == StubConventionType.SMART_FINAL.name or \
+            self._type == StubConventionType.NONE.name
+
+    def is_calculate_backwards(self):
+        return self._type == StubConventionType.SHORT_INITIAL.name or \
+            self._type == StubConventionType.LONG_INITIAL.name or \
+            self._type == StubConventionType.SMART_INITIAL.name
+
+    @staticmethod
+    def implied_roll_convention(dt1: datetime, dt2: datetime, freq: Frequency, eom: bool) -> RollConvention:
+        if freq.is_month_based():
+            if eom and dt1.day == calendar.monthrange(dt1.year, dt1.month)[1]:
+                return RollConvention(RollConventionType.EOM)
+            return RollConvention(getattr(RollConventionType, f'DAY_{dt1.day}'))
+        else:
+            return RollConvention(RollConventionType.NONE)
+
+    def to_roll_convention(self, start: datetime, end: datetime, freq: Frequency, eom: bool) -> RollConvention:
+        if self._type == RollConventionType.NONE.name and freq.is_month_based():
+            if start.day != end.day and (start.day == calendar.monthrange(start.year, start.month)[1] or
+                                         end.day == calendar.monthrange(end.year, end.month)[1]):
+                if eom:
+                    return RollConvention(RollConventionType.EOM)
+                else:
+                    return RollConvention(getattr(RollConventionType, f"DAY_{max(start.day, end.day)}"))
+        if self.is_calculate_backwards():
+            return self.implied_roll_convention(end, start, freq, eom)
+        else:
+            return self.implied_roll_convention(start, end, freq, eom)
